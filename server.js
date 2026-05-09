b179be167ca8c0de6c0538aeb4d1170971c84079
shabbir khargon <shabbir53khargon@gmail.com>
2026-05-09 19:46:39 +0530

    changes for server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const errorHandler = require('./api/middlewares/errorHandler');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 // ==================== DB ====================
 connectDB();
