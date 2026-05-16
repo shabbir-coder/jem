aa34bc0ecf4e6df7abe2f940df2c83886aaaa5c6
shabbir khargon <shabbir53khargon@gmail.com>
2026-05-16 13:36:44 +0530

    change in browserjs

--- api/config/browser.js
+++ api/config/browser.js
@@ -1,3 +1,6 @@
+process.env.PLAYWRIGHT_BROWSERS_PATH =
+  '/home/site/wwwroot/ms-playwright';
+
 const { chromium } = require('playwright');
 
 let browserInstance = null;
@@ -6,19 +9,30 @@ async function getBrowser() {
 
   try {
 
+    // Reuse existing browser instance
     if (browserInstance) {
       return browserInstance;
     }
 
     browserInstance = await chromium.launch({
-      headless: true
-    });
 
-    browserInstance.on('disconnected', () => {
-      browserInstance = null;
+      headless: true,
+
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage'
+      ]
     });
 
     console.log('✅ Playwright browser launched');
+
+    browserInstance.on('disconnected', () => {
+
+      console.log('⚠️ Browser disconnected');
+
+      browserInstance = null;
+    });
 
     return browserInstance;
 
